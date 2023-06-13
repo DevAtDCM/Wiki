f1fd291de3b0236306a2bc5bba9868bc9b36b716
Alex <alex.mills@digitalcm.com>
2023-06-13 11:18:06 -0400

    Test docker stuff

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+node server
 
 /usr/bin/ssh-keygen -A
 mkdir -p /var/run/sshd
@@ -7,4 +8,4 @@ mkdir -p /var/run/sshd
 /wiki/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 /wiki/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=azure-app
 echo Tailscale started
-ALL_PROXY=socks5://localhost:1055/ /wiki/my-app
+ALL_PROXY=socks5://localhost:1055/ /wiki/
