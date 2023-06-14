9086828bc64906e907e3b48ce4111f368d0422bd
Alex <alex.mills@digitalcm.com>
2023-06-14 13:45:30 -0400

    i quit

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 /wiki/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 /wiki/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=Wiki-Page
 echo Tailscale started
-#sudo ALL_PROXY=socks5://localhost:1055/ /wiki/my-app
+ALL_PROXY=socks5://localhost:1055/ #/wiki/my-app
